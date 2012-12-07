3d0aebb64c4ea636ed40e544712215fc970699db
hlb <hlb@handlino.com>
2012-12-07 15:58:50 +0800

    use crypto-js to get avatars from avatars.io

--- javascripts/app.js
+++ javascripts/app.js
@@ -38,21 +38,34 @@ var highlightText = function() {
 
 $(function() {
 
+    var avatars = [];
+    window.avatars = avatars;
+
     $.getJSON(JSON_URL, function(data) {
         $.each(data, function(i) {
             if (this[0].type === 'interp') {
+                var $ul, $l, avatar_pos;
                 $('#log').append('<hr>');
 
-                var $ul = $('<ul>');
+                $ul = $('<ul>');
                 $.each(this[0].people, function() {
                     $ul.append('<li>' + this + '</li>');
                 });
                 $('#log').append($ul);
 
-                var $l = $('<div>');
+                $l = $('<div>');
 
                 $.each(this[1], function(j) {
-                    $l.append('<div class="media"><a class="avatar pull-left" href="#"><img class="media-object" src="http://placehold.it/100"></a><div class="media-body"><h4 class="media-heading">' + this.speaker + '</h4><p id="p' + i + '-' + j + '">' + this.content + '<a href="#p' + i + '-' + j + '">#</a></p></div></div>');
+                    var speaker = this.speaker.replace(/(委員|院長|主任)+/, "");
+                    avatar_pos = $.inArray("MLY/" + speaker, avatars);
+
+                    if (avatar_pos < 0) {
+
+                        avatars.push("MLY/" + speaker);
+                        avatars.push(CryptoJS.MD5("MLY/" + speaker).toString());
+                        avatar_pos = avatars.length - 2;
+                    }
+                    $l.append('<div class="media"><a class="avatar pull-left" href="#"><img class="media-object" src="http://avatars.io/50a65bb26e293122b0000073/'+ avatars[avatar_pos+1] + '"></a><div class="media-body"><h4 class="media-heading">' + this.speaker + '</h4><p id="p' + i + '-' + j + '">' + this.content + '<a href="#p' + i + '-' + j + '">#</a></p></div></div>');
                 });
                 $('#log').append($l);
             }
